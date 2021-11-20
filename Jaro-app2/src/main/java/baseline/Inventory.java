b201a484542ae462e86cc6b10248fcfd3e406217
mathdude3482 <orajsamej@gmail.com>
2021-11-20 08:47:32 -0500

    Initial implementation of inventory class. Updated diagram accordingly.

--- Jaro-app2/src/main/java/baseline/Inventory.java
+++ Jaro-app2/src/main/java/baseline/Inventory.java
@@ -4,26 +4,38 @@
  */
 package baseline;
 public class Inventory {
+    private String name;
+    private String serialNumber;
+    private double value;
+
+    public Inventory(String name, String serialNumber, double value) {
+        this.name = name;
+        this.serialNumber = serialNumber;
+        this.value = value;
+    }
     //this class will contain only set and get methods for each part of an item.
-    public void setName(){
+    public void setName(String name){
         //set the name of the item.
+        this.name = name;
     }
-    public void setSerial(){
+    public void setSerial(String serialNumber){
         //set the Serial number of the item.
+        this.serialNumber = serialNumber;
     }
-    public void setValue(){
+    public void setValue(double value){
         //set the value of the item.
+        this.value = value;
     }
     public String getName(){
         //get the name of the item.
-        return "";
+        return name;
     }
     public String getSerial(){
         //get the Serial number of the item.
-        return "";
+        return serialNumber;
     }
-    public String getValue(){
+    public double getValue(){
         //get the value of the item.
-        return "";
+        return value;
     }
 }
